b64f7a1e925c1a25114941f895c47040c30ae079
ChangSpring <ChangSpring0728@gmail.com>
2016-06-14 18:33:02 +0800

    添加base 的class

--- app/src/main/java/com/alfred/chowder/ui/base/BaseFragment.java
+++ app/src/main/java/com/alfred/chowder/ui/base/BaseFragment.java
@@ -31,22 +31,8 @@ public class BaseFragment extends Fragment {
 
 	private OnFragmentInteractionListener mListener;
 
-	/**
-	 * Use this factory method to create a new instance of
-	 * this fragment using the provided parameters.
-	 *
-	 * @param param1 Parameter 1.
-	 * @param param2 Parameter 2.
-	 * @return A new instance of fragment BaseFragment.
-	 */
-	// TODO: Rename and change types and number of parameters
-	public static BaseFragment newInstance(String param1, String param2) {
-		BaseFragment fragment = new BaseFragment();
-		Bundle args = new Bundle();
-		args.putString(ARG_PARAM1, param1);
-		args.putString(ARG_PARAM2, param2);
-		fragment.setArguments(args);
-		return fragment;
+	public BaseFragment() {
+		// Required empty public constructor
 	}
 
 	/**
@@ -65,8 +51,6 @@ public class BaseFragment extends Fragment {
 		args.putString(ARG_PARAM2, param2);
 		fragment.setArguments(args);
 		return fragment;
-	}	public BaseFragment() {
-		// Required empty public constructor
 	}
 
 	@Override
@@ -115,7 +99,7 @@ public class BaseFragment extends Fragment {
 	 * fragment to allow an interaction in this fragment to be communicated
 	 * to the activity and potentially other fragments contained in that
 	 * activity.
-	 * <p>
+	 * <p/>
 	 * See the Android Training lesson <a href=
 	 * "http://developer.android.com/training/basics/fragments/communicating.html"
 	 * >Communicating with Other Fragments</a> for more information.
